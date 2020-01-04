997e491029c7084d301a42e7f02729d91668af67
Kyryl Stronko <kirstronko@gmail.com>
2020-01-04 22:36:13 +0200

    prod config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,93 +4,106 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const Dotenv = require('dotenv-webpack');
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
-const cssLoaders = [{
-    loader: MiniCssExtractPlugin.loader, 
-  }, { 
-    loader: 'css-loader', 
-    options: {
-      modules: true,    
-      localIdentName: '[name]__[local]___[hash:base64:5]',
-      sourceMap: true
-    } 
-  }
-];
+module.exports = (env, argv) => {  
+  const isDevelopment = argv.mode === 'development';
 
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'bundle.min.js',
-    publicPath: '/'
-  },
-  resolve: {
-    plugins: [
-      new TsconfigPathsPlugin(),
-    ],
-    extensions: ['.ts', '.tsx', '.js', '.css', '.js.map'],    
-    symlinks: false,
-    alias: {
-      'react': path.resolve('./node_modules/react'),
-      'react-dom': path.resolve('./node_modules/react-dom'),      
+  const cssLoaders = [{
+      loader: MiniCssExtractPlugin.loader, 
+    }, { 
+      loader: 'css-loader', 
+      options: {
+        modules: true,    
+        localIdentName: '[name]__[local]___[hash:base64:5]',
+        sourceMap: true
+      } 
     }
-  },
-  module: {
-    rules: [
-      { 
-        test: /\.tsx?$/, 
-        loader: 'ts-loader',
-        options: {
-          allowTsInNodeModules: true,
+  ];
+
+  const outputPath = isDevelopment ? 
+    path.join(__dirname, '/dist') : 
+    path.join(__dirname, '../static/client');
+
+  const filename = isDevelopment ?
+    'bundle.min.js' :
+    'bundle-[hash].min.js';
+
+  const publicPath = isDevelopment ?
+    '/' :
+    '/static/client/';
+
+  return {
+    entry: './src/index.tsx',
+    output: {
+      path: outputPath,
+      filename,
+      publicPath,
+    },
+    resolve: {
+      plugins: [
+        new TsconfigPathsPlugin(),
+      ],
+      extensions: ['.ts', '.tsx', '.js', '.css', '.js.map'],    
+      symlinks: false,
+      alias: {
+        'react': path.resolve('./node_modules/react'),
+        'react-dom': path.resolve('./node_modules/react-dom'),      
+      }
+    },
+    module: {
+      rules: [
+        { 
+          test: /\.tsx?$/, 
+          loader: 'ts-loader',
+        },      
+        {
+          test: /\.css$/,
+          use: cssLoaders,
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            ...cssLoaders,
+            { loader: 'sass-loader', options: { sourceMap: true } }
+          ],
+        },
+        {
+          test: /\.js\.map$/,
+          use: "source-map-loader",
+          enforce: "pre",
+          include: path.join(__dirname, '/node_modules/@micelord')
         }
-      },      
-      {
-        test: /\.css$/,
-        use: cssLoaders,
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          ...cssLoaders,
-          { loader: 'sass-loader', options: { sourceMap: true } }
-        ],
-      },
-      {
-        test: /\.js\.map$/,
-        use: "source-map-loader",
-        enforce: "pre",
-        include: path.join(__dirname, '/node_modules/@micelord')
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      }),
+      new MiniCssExtractPlugin({      
+        filename: "style.css",
+        chunkFilename: "[name].css"
+      }),    
+      new Dotenv({
+        path: './.env',
+      })
+    ],
+    devServer: {
+      port: 3000,
+      historyApiFallback: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3001',
+          secure: false
+        },
+        '/auth': {
+          target: 'http://localhost:3001',
+          secure: false
+        },
+        '/static': {
+          target: 'http://localhost:3001',
+          secure: false
+        }
       }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
-    new MiniCssExtractPlugin({      
-      filename: "style.css",
-      chunkFilename: "[name].css"
-    }),    
-    new Dotenv({
-      path: './.env',
-    })
-  ],
-  devServer: {
-    port: 3000,
-    historyApiFallback: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        secure: false
-      },
-      '/auth': {
-        target: 'http://localhost:3001',
-        secure: false
-      },
-      '/static': {
-        target: 'http://localhost:3001',
-        secure: false
-      }
-    }
-  },
-  devtool: 'eval-source-map',
+    },
+    devtool: isDevelopment ? 'eval-source-map' : undefined,
+  }
 }
