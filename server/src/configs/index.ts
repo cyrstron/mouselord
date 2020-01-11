56db7543e37c9788b93cb8b25be559857521d936
Kyryl Stronko <kyrylstronko@Kyryl-Stronko.local>
2020-01-12 01:01:47 +0200

    config to base 64

--- server/src/configs/index.ts
+++ server/src/configs/index.ts
@@ -11,8 +11,8 @@ export const utilsConfig = {
     appSecret: process.env.FB_APP_SECRET,
   },
   jwt: {
-    privateKey: process.env.JWT_PRIVATE_KEY,
-    publicKey: process.env.JWT_PUBLIC_KEY,
+    privateKey: Buffer.from(process.env.JWT_PRIVATE_KEY, 'base64').toString('utf8'),
+    publicKey: Buffer.from(process.env.JWT_PUBLIC_KEY, 'base64').toString('utf8'),
     expiration: '24h',
     encryption: 'RS256',
     issuer: 'MouseLord',
